38b3a2563a1562318599302fc40910e651b25ecb
Jonathan <joandste@abo.fi>
2024-04-13 21:20:14 +0300

    user/labwc: new package (0.7.1)

---
+++ user/labwc/template.py
@@ -0,0 +1,28 @@
+pkgname = "labwc"
+pkgver = "0.7.1"
+pkgrel = 1
+build_style = "meson"
+hostmakedepends = [
+    "meson",
+    "pkgconf",
+    "scdoc",
+]
+makedepends = [
+    "cairo-devel",
+    "libxml2-devel",
+    "pango-devel",
+    "wayland-devel",
+    "wayland-protocols",
+    "wlroots0.17-devel",
+]
+pkgdesc = "Wayland window-stacking compositor"
+maintainer = "joandste <joandste@abo.fi>"
+license = "GPL-2.0-or-later"
+url = "https://labwc.github.io"
+source = f"https://github.com/labwc/labwc/archive/{pkgver}.tar.gz"
+sha256 = "1810ec55e287708e7a3cd44c726aa887db02480704db82b3d0bd550a6c4bfb76"
+hardening = ["vis", "cfi"]
+
+
+def post_install(self):
+    self.install_license("LICENSE")
